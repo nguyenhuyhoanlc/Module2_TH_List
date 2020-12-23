8ce7d74b1b62db49b779373614e33ac71d739702
nguyenhuyhoanlc <nguyenhuyhoanlc@gmail.com>
2020-12-23 22:33:02 +0700

    Thuc hanh

---
+++ src/Mylist.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Mylist<E> {
+    private int size =0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object element[];
+
+    public Mylist(){
+        element = new Object[DEFAULT_CAPACITY];
+    }
+
+    private void ensureCapa(){
+        int newSize = element.length*2;
+        element = Arrays.copyOf(element, newSize);
+    }
+
+    public void add(E e){
+        if ( size == element.length) {
+            ensureCapa();
+        }
+        element[size++] = e;
+    }
+
+    public E get(int i){
+        if (i >= size || i < 0){
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i );
+        }
+        return (E)element[i];
+    }
+
+
+}
+
